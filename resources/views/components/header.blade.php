51123e5ee74d21be232cf5279597b387cf67da25
francesco <francescosanti91@gmail.com>
2022-01-21 16:10:44 +0100

    creazione header

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,42 @@
+<header>
+    <div class="container">
+        <div id="logo">
+            {{-- <img src="../assets/img/dc-logo.png" alt="logo dc comics"> --}}
+            <div>Logo DC</div>
+        </div>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">Characters</a>
+                </li>
+                <li>
+                    <a href="#">Comics</a>
+                </li>
+                <li>
+                    <a href="#">Movies</a>
+                </li>
+                <li>
+                    <a href="#">tv</a>
+                </li>
+                <li>
+                    <a href="#">games</a>
+                </li>
+                <li>
+                    <a href="#">collectibles</a>
+                </li>
+                <li>
+                    <a href="#">videos</a>
+                </li>
+                <li>
+                    <a href="#">fans</a>
+                </li>
+                <li>
+                    <a href="#">news</a>
+                </li>
+                <li>
+                    <a href="#">shop</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
