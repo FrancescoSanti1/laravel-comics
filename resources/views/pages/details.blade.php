fafbc35120450204ec4e394a70961b8c98fe44ee
francesco <francescosanti91@gmail.com>
2022-01-24 17:10:22 +0100

    aggiunti altri elementi pagina details

--- resources/views/pages/details.blade.php
+++ resources/views/pages/details.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.main-layout')
 @section('main-content')
 
+<div class="stripe">
+    <div class="container">
+        <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+    </div>
+</div>
+
 <div id="details-top">
     <div class="container">
         <div class="description">
@@ -8,16 +14,36 @@
             <div class="availability">check availability</div>
             <div class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium provident omnis expedita harum, quia alias voluptatum asperiores veritatis, earum tenetur iste, voluptates inventore itaque praesentium nostrum? Excepturi doloribus mollitia consequatur ad natus iste ea dolorum itaque distinctio magnam eaque officia voluptatum animi, repellat voluptatem repellendus quis enim. Corporis recusandae distinctio nesciunt repellat dicta in blanditiis, hic numquam debitis cum eveniet quidem accusantium? Rem consequuntur vitae vero obcaecati velit aut rerum ea corrupti temporibus minus, amet modi eius ut ad quibusdam blanditiis ratione veritatis, harum cumque doloremque nemo repellendus numquam? Consequuntur vero perferendis quaerat, exercitationem nemo illum corrupti laudantium! Nemo, veritatis!</div>
         </div>
-        <div class="adv">advertising</div>
+        <div class="adv">advertisement</div>
     </div>
 </div>
 <div id="details-bottom">
     <div class="container">
-        <div class="talent">
+        <div class="spec">
             <div class="title">Talent</div>
+            <div class="row">
+                <div class="title">Art by:</div>
+                <div class="text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati deleniti laboriosam odio maxime molestias vitae, minus illo quo dolorem voluptate?</div>
+            </div>
+            <div class="row">
+                <div class="title">Written by:</div>
+                <div class="text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati deleniti laboriosam odio maxime molestias vitae, minus illo quo dolorem voluptate?</div>
+            </div>
         </div>
-        <div class="specs">
+        <div class="spec">
             <div class="title">Specs</div>
+            <div class="row">
+                <div class="title">Series:</div>
+                <div class="text">action comics</div>
+            </div>
+            <div class="row">
+                <div class="title">U.S. price:</div>
+                <div class="text">19.00$</div>
+            </div>
+            <div class="row">
+                <div class="title">On sale date:</div>
+                <div class="text">Oct 02 2018</div>
+            </div>
         </div>
     </div>
 </div>
